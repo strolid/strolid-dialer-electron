3ed956c3cd3785cbc73ce2e4ae29c34eacc2e7d8
Pavan Kumar <pavanputhra@gmail.com>
2024-01-24 09:21:47 +0530

    Added CORS header

--- httpServer.js
+++ httpServer.js
@@ -5,13 +5,23 @@ const PORT = process.env.PORT || 3020;
 
 exports.startServer = function () {
   const server = http.createServer((req, res) => {
-    // Check if the request path is /ping
-    if (req.url === '/ping') {
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('PONG FROM STROLID DIALER');
+    // Allow requests from any origin
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+      res.writeHead(204);
+      res.end();
     } else {
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Not Found');
+      // Check if the request path is /ping
+      if (req.url === '/ping') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('PONG FROM STROLID DIALER');
+      } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('Not Found');
+      }
     }
   });
 
@@ -19,4 +29,5 @@ exports.startServer = function () {
   server.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
   });
+
 }
