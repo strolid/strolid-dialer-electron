16879b02e4cf8694430f2c0f94c71efbf7f847a4
Scott Henshaw <scott.henshaw86@gmail.com>
2026-02-25 18:12:22 +0900

    Fix DTLS issues caused by too many ICE candidates

--- main.js
+++ main.js
@@ -152,6 +152,7 @@ function createWindow() {
 
     })
     win.webContents.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36');
+    win.webContents.setWebRTCIPHandlingPolicy('default_public_interface_only');
     // ENABLE THIS TO OPEN DEV TOOLS ON START
 
     // Set window title with version
